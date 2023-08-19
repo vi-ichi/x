56c0cf193d6111b4ac776dd1fb584be71f582feb
Vi <71643953+vi-ichi@users.noreply.github.com>
2023-08-19 14:45:10 +0000

    dev

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,33 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { useEffect, useState } from "react";
+import AnimatedCursor from "react-animated-cursor";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [client, setClient] = useState(false);
+
+  useEffect(() => {
+    setClient(true);
+  }, []);
+
+  return (
+    <>
+      {client && (
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={35}
+          innerScale={1}
+          outerScale={2}
+          outerAlpha={0}
+          hasBlendMode={true}
+          innerStyle={{
+            backgroundColor: "white",
+          }}
+          outerStyle={{
+            border: "3px solid white",
+          }}
+        />
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
