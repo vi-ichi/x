871b9b88535261487375d397b079c6963fb183dc
Vi <71643953+vi-ichi@users.noreply.github.com>
2023-08-23 12:29:58 +0000

    docs

--- components/Canvas.js
+++ components/Canvas.js
@@ -11,6 +11,8 @@ export default function Canvas() {
     const c = ref.current.getContext("2d");
     c.fillStyle = "black";
     c.fillRect(0, 0, 1000, 1000);
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
   }, []);
 
   return <canvas height={200} width={200} className="ml-auto pb-2 pr-2" ref={ref}></canvas>;
