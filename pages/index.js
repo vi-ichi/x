0007ae02c5b9a039fdf31f97575defe0a83af971
Vi <71643953+vi-ichi@users.noreply.github.com>
2023-08-27 21:38:33 +0000

    spotify

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,56 @@ export default function Home() {
         <div className="text-2xl">{level.title}</div>
         <div>{level.author}</div>
       </div>
-      <div className="absolute text-center w-full bottom-0 mb-8">press SPACE to play</div>
+      <div className="absolute flex justify-center w-full bottom-0 mb-16 gap-8">
+        <div className="rounded-full bg-gray-300 p-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="black"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z"
+            />
+          </svg>
+        </div>
+        <div className="rounded-full bg-gray-300 p-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="black"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+            />
+          </svg>
+        </div>
+        <div className="rounded-full bg-gray-300 p-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="black"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z"
+            />
+          </svg>
+        </div>
+      </div>
       <canvas height={height} width={width} ref={ref}></canvas>
     </>
   );
